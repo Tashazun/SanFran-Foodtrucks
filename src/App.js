a205ec2a0eea89ae04e7168370c6fa33d446e7a9
Tasha R Zuniga <Tashazun@gmail.com>
2019-02-21 11:34:53 -0800

    basic layout setup

--- src/App.js
+++ src/App.js
@@ -1,41 +1,24 @@
 import React, { Component } from 'react';
-import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
-import 'leaflet/dist/leaflet.css';
-import L from 'leaflet';
+import MyMap from './MyMap';
 
-delete L.Icon.Default.prototype._getIconUrl;
-
-L.Icon.Default.mergeOptions({
-    iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
-    iconUrl: require('leaflet/dist/images/marker-icon.png'),
-    shadowUrl: require('leaflet/dist/images/marker-shadow.png')
-});
-
+import './App.css';
 class App extends Component {
-
-  state = {
-    lat: 51.505,
-    lng: -0.09,
-    zoom: 13,
-  }
-
-  render() {
-    const position = [this.state.lat, this.state.lng];
-
-    return (
-      <Map center={position} zoom={this.state.zoom} style={{height: '300px'}}>
-        <TileLayer
-          attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-        <Marker position={position}>
-          <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup>
-        </Marker>
-      </Map>
-    )
-  }
+    render() {
+        return (
+            <div id='App-container'>
+                <header id='App-header'>
+                    <h2>Mobile Eats</h2>
+                    <h4>Truck tracker for the San Franciscan foodie.</h4>
+                </header>
+                <section id='App-map-frame'>
+                    <MyMap />
+                </section>
+                <footer id='App-footer'>
+                    footer
+                </footer>
+            </div>
+        );
+    }
 }
 
 export default App;
