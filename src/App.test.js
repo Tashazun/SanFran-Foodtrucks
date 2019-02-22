c12ede46f0b84da9b4c92d190512af896abef247
Tasha R Zuniga <Tashazun@gmail.com>
2019-02-21 16:00:40 -0800

    first test on App data fetch successful

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { shallow } from 'enzyme';
+
+
+
+describe('App', () => {
+  it('calls fetch with successful status', () => {
+    const mockSuccessResponse = {};
+    const mockJsonPromise = Promise.resolve(mockSuccessResponse);
+    const mockFetchPromise = Promise.resolve({
+      json: () => mockJsonPromise,
+    });
+    jest.spyOn(global, 'fetch').mockImplementation(() => mockFetchPromise);
+    
+    const wrapper = shallow(<App />);
+    
+    expect(global.fetch).toHaveBeenCalledTimes(1);
+    expect(global.fetch).toHaveBeenCalledWith('https://data.sfgov.org/resource/bbb8-hzi6.json');
+  })
+  
+  it('resets the state with the fetched data', () => {
+    
+  })
+
+  it('sets an error when the fetch fails', () => {
+
+  })
+})
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
