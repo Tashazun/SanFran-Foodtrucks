9f4a48cb355b245b23c1827c4e3784d22645c475
Tasha R Zuniga <Tashazun@gmail.com>
2019-02-26 11:30:49 -0800

    small change to server

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const path = require("path");
 const app = express();
+const PORT = process.env.PORT || 8080;
 
 app.use(express.static(path.join(__dirname, "build")));
 
@@ -8,4 +9,4 @@ app.get("/", function(req, res) {
   res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
-app.listen(9000);
+app.listen(PORT, () => console.log(`Listening on ${PORT}`));
